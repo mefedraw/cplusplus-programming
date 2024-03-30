c5399c14f82d36452dc9495740f1b05202d250c5
Agibalov Ilya Stanislavovich <59418574+IlyaAGL@users.noreply.github.com>
2024-03-30 03:39:37 +0300

    Fixed bugs

--- seventh/circularBuffer.h
+++ seventh/circularBuffer.h
@@ -1,62 +1,77 @@
-#include <iterator>
-
-// I didn't run it, so if there are mistakes - pull request
+#include <iostream>
 
 template <typename T>
 class CircularBuffer
 {
 private:
-    T *_data, ptr_begin, ptr_end, begin, end;
+    T* _data;
+    T* ptr_start;
+    T* start;
+    T* ptr_finish;
+    T* finish;
     unsigned int buff_size, buff_capacity;
 
 public:
     class iter : public std::iterator<std::random_access_iterator_tag, T>
     {
     private:
-        T *ptr;
+        T* ptr;
 
     public:
-        iter &operator++()
+
+        explicit iter(T* start)
         {
-            ptr = (ptr + 1) % buff_capacity;
+            ptr = start;
+        }
+        iter(const iter& obj)
+        {
+            ptr = obj.ptr;
+        }
+        iter() {
+            ptr = nullptr;
+        }
+
+        ~iter() = default;
+
+        iter& operator++()
+        {
+            ptr = ptr + 1;
             return *this;
         }
 
-        iter &operator--()
+        iter& operator--()
         {
-            ptr = (ptr - 1 + buff_capacity) % buff_capacity;
+            ptr = ptr - 1;
             return *this;
         }
 
-        iter &operator+=(std::ptrdiff_t b)
+        iter& operator+=(std::ptrdiff_t b)
         {
-            ptr = (ptr + b) % buff_capacity;
+            ptr = ptr + b;
             return *this;
         }
 
-        iter &operator-=(std::ptrdiff_t b)
+        iter& operator-=(std::ptrdiff_t b)
         {
-            ptr = (ptr - b + buff_capacity) % buff_capacity;
+            ptr = ptr - b;
             return *this;
         }
 
-        iter operator+(std::ptrdiff_t b) const
+        iter operator+(std::ptrdiff_t b)
         {
-            iter newIter = *this;
-            newIter.ptr = (ptr + b) % buff_capacity;
-            return newIter;
+            ptr = ptr + b;
+            return *this;
         }
 
-        iter operator-(std::ptrdiff_t b) const
+        iter operator-(std::ptrdiff_t b)
         {
-            iter newIter = *this;
-            newIter.ptr = (ptr - b + buff_capacity) % buff_capacity;
-            return newIter;
+            ptr = ptr - b;
+            return *this;
         }
 
-        std::ptrdiff_t operator-(const iter &b) const
+        std::ptrdiff_t operator-(const iter& b) const
         {
-            return b.ptr - ptr;
+            return ptr - b.ptr;
         }
 
         T& operator[](std::ptrdiff_t b) const
@@ -64,83 +79,117 @@ public:
             return *(ptr + b);
         }
 
-        bool operator==(const iter &other) const
+        T& operator*() {
+            return *ptr;
+        }
+
+        T* operator->() {
+            return ptr;
+        }
+
+        bool operator==(const iter& other) const
         {
             return ptr == other.ptr;
         }
-        bool operator!=(const iter &other) const
+        bool operator!=(const iter& other) const
         {
             return ptr != other.ptr;
         }
-        bool operator<(const iter &other) const
+        bool operator<(const iter& other) const
         {
             return ptr < other.ptr;
         }
-        bool operator>(const iter &other) const
+        bool operator>(const iter& other) const
         {
             return ptr > other.ptr;
         }
-        bool operator<=(const iter &other) const
+        bool operator<=(const iter& other) const
         {
             return ptr <= other.ptr;
         }
-        bool operator>=(const iter &other) const
+        bool operator>=(const iter& other) const
         {
             return ptr >= other.ptr;
         }
     };
 
+    // Accessing first and last positions
+
+    iter begin() const {
+        return iter(_data);
+    }
+
+    iter end() const {
+        return iter(_data + buff_capacity);
+    }
+
+    // Constructors and destructor
+
     CircularBuffer() : buff_capacity(0), buff_size(0), _data(nullptr) {}
 
     CircularBuffer(int cap) : buff_capacity(cap), buff_size(0)
     {
-        _data = new T[buff_capacity];
-        ptr_begin = begin = _data;
-        ptr_end = end = _data + buff_capacity;
+        _data = new T[cap];
+        ptr_start = _data;
+        start = _data;
+        ptr_finish = _data + cap - 1;
+        finish = _data + cap - 1;
     }
 
-    // Add and delete from start and end
+    ~CircularBuffer() = default;
 
-    void add_end(T el)
+    // Adding element to start and end 
+
+    void add_start_actualy(T el)
     {
-        *ptr_end = el;
-        ptr_end = (end - 1 + buff_capacity) % buff_capacity;
+        *ptr_start = el;
+        ptr_start = _data + (ptr_start - _data + 1) % buff_capacity;
         ++buff_size;
     }
 
-    void add_start(T el)
+    void add_start(T el) {
+        add_start_actualy(el);
+    }
+
+    void add_finish_actualy(T el)
     {
-        *ptr_begin = el;
-        ptr_begin = (begin + 1) % buff_capacity;
+        *ptr_finish = el;
+        ptr_finish = _data + (ptr_finish - _data - 1 + buff_capacity) % buff_capacity;
         ++buff_size;
     }
+
+    void add_finish(T el) {
+        add_finish_actualy(el);
+    }
+
+    // Deliting elements from start and end
 
     void delete_start()
     {
-        ++begin;
+        ++start;
         --buff_size;
     }
 
-    void delete_end()
+    void delete_finish()
     {
-        --end;
+        --finish;
         --buff_size;
     }
 
     // Adding and deleting by iterator
 
-    void add_by_it(iter el)
+    void add_by_it(iter el, T num)
     {
-        *((begin + el) % buff_capacity) = *el;
+        *el = num;
     }
 
     void delete_by_it(iter el)
     {
-        
-        while(el != end) {
+
+        while (el != finish) {
             _data[el] = _data[el + 1];
 
-            if(el + 1 == end) {
+            if (el + 1 == finish) {
                 break;
             }
 
@@ -152,33 +201,44 @@ public:
 
     // Getting first and last elements
 
-    T get_start()
+    T* get_start()
     {
-        return *begin;
+        return start;
     }
 
-    T get_end()
+    T* get_finish()
     {
-        return *end;
+        return finish;
     }
+
 
     // Accessing by index
 
     T get_by_ind(size_t ind)
     {
-        return *((begin + ind) % buff_capacity);
+        return _data[(start - _data + ind) % buff_capacity];
     }
 
     // Changing capacity
 
-    CircularBuffer change_cap(size_t cap) {
+    CircularBuffer change_cap(int cap) {
         CircularBuffer newBuff(cap);
+        T* new_start = start;
 
-        while(begin != end) {
-            newBuff.add_start(*begin);
-            ++begin;
+        while (new_start != finish) {
+            newBuff.add_start(*new_start);
+            ++new_start;
         }
 
+        newBuff.buff_size = buff_size;
+
         return newBuff;
+    }
+
+    unsigned int capacity() {
+        return buff_capacity;
+    }
+    unsigned int size() {
+        return buff_size;
     }
 };
